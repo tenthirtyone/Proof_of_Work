4b04b94f7b908163682bcf2597cd60618e10c239
tenthirtyone <spactigus@gmail.com>
2015-09-08 17:45:51 -0400

    flipped 512 -> 256

--- proof_of_work/pow.py
+++ proof_of_work/pow.py
@@ -10,15 +10,16 @@ message = "This is a random message." 	# Plaintext message
 nonce = 0				# 
 guess = 99999999999999999999
 payload = timestamp + message
-throttle = 10000000
+throttle = 100000000
 target = 2**64 / throttle
 
-payloadHash = hashlib.sha512(payload).digest()
+payloadHash = hashlib.sha256(payload).digest()
 
 start = time.time()
 while guess > target:
 	nonce += 1
-	guess, = unpack('>Q',hashlib.sha512(hashlib.sha512(pack('>Q',nonce) + payloadHash).digest()).digest()[0:8])
+	guess, = unpack('>Q',hashlib.sha256(hashlib.sha256(pack('>Q',nonce) + payloadHash).digest()).digest()[0:8])
+	print(guess);
 end = time.time()
 
 print "%s:%s:%s:%s:%s:%s:%s" % (timestamp, message, nonce, guess, payload, target, end-start)
