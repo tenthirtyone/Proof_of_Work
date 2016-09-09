7e396fa6ca68b707322441a05ba23db14c6b8ad3
tenthirtyone <Spactigus@gmail.com>
2016-09-08 20:45:29 -0400

    easy reader

--- proof_of_work/pow.js
+++ proof_of_work/pow.js
@@ -6,14 +6,31 @@ var message = "Random Message";
 var nonce = 0;
 guess = 99999999999999999999;
 payload = timestamp + message;
+
 throttle = 1000000;
 target = Math.pow(2, 64) / throttle;
 
 var start = Date.now();
+
+
 while (guess > target) {
-    hash = crypto.createHash("sha256");
+    hashA = crypto.createHash("sha256");
+    hashB = crypto.createHash("sha256");
     nonce+=1;
-    guess = parseInt(hash.update(payload + nonce).digest('hex').substring(0,16), 16);
+    guess = parseInt(
+              hashB.update(
+                hashA.update(payload + nonce).digest('hex'))
+              .digest('hex') 
+            .substring(0,16), 16);
+    // Turn on to debug
+    //guess = hash.update(payload + nonce).digest('hex');
+
+    // 16 Hex nibbles
+    console.log(guess)
+    //console.log(guess.substring(0,16));
+    // 16 x 4 bits = 64 bits in decimal
+    //console.log(parseInt(guess.substring(0,16), 16));
+    //console.log(target)
 }
 var end = Date.now()
 console.log("Run time: ", (end-start)/1000);
